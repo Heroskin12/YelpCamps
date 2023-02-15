16dca8e454c53631cafa7ba447e24fb021b40c7e
Jamie Furlong <jamiefurlong16@gmail.com>
2023-02-15 13:24:21 +0000

    Set up the Mongo database with mongoose. Confirmed the db works locally. Created new GET route to check entries are successfully being added. Campground.js created in models directory to contain db information. Imported Campground.js into app.js

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const campground = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected!");
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new campground({title: 'My Backyard', price: 50, description: "Cheap camping!", location: "Swansea"});
+    await camp.save();
+    res.send(camp);
 })
 app.listen(3000, () => {
     console.log("Listening on port 3000!");
