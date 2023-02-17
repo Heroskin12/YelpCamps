b97500cd65fd3880fac1c869b87b5a7eafc4256a
Jamie Furlong <jamiefurlong16@gmail.com>
2023-02-17 21:40:25 +0000

    Integrated Bootstrap Design and Configured Error Handling
    Basic design implemented with Bootstrap 5 on all pages except Home.
    Used JOI to handle server-side validation for adding / editing campgrounds.
    Set up default 404 error and a custom error-logging template.
    Client-side validation set with Bootstrap validation classes and JS.
    Bootstrap JS script added to Boilerplate.

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
-const engine = require('ejs-mate')
+const engine = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const {campgroundSchema } = require('./schemas.js');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -27,53 +30,72 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(morgan('common'));
 
+const validateCampground = (req, res, next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(400, msg)
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-})
+    res.render('campgrounds/index', {campgrounds});
+}));
 
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
-})
+});
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+    res.redirect(`/campgrounds/${campground._id}`); 
+}));
 
-})
-
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const camp = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {camp})
-})
+}));
 
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const deletedCamp = await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');    
-})
+}));
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', {campground})
-})
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const {id} = req.params;
     const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {runValidators: true, new: true});
     console.log(camp);
     res.redirect(`/campgrounds/${id}`);
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError(404, 'Page Not Found'))
 })
 
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = "Oh no! Something went wrong!"
+    res.status(statusCode).render('error', {err});
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!");
-})
+});
+
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const {places, descriptors} = require('./seedHelpers')
 const cities = require('./cities');
+const axios = require('axios');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -16,13 +17,43 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+async function seedImg(random10) {
+    try {
+        const res = await axios.get('https://api.pexels.com/v1/search', {
+            headers: {
+                Accept: "application/json",
+                Authorization: ""
+            },
+            params: {
+                query: 'camping',
+                orientation: 'landscape',
+                size: 'medium'
+            }
+
+        })
+        // console.log(res.data.photos[random10].src.original);
+        return res.data.photos[random10].src.original;
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 5; i++) {
+        const price = Math.floor(Math.random()*20)+10;
         const random1000 = Math.floor(Math.random()*1000);
+        const random10 = Math.floor(Math.random()*10);
+        
+    
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, at suscipit! Tempora suscipit, iure assumenda enim nulla similique provident in!',
+            price,
+            image: await seedImg(random10)
+            
         })
         await camp.save(); 
     }
